e1353928e5bc298c6caf476a0b0c41b31d13336f
Do Minh Hieu <dominhhieu381@gmail.com>
2024-03-27 13:26:09 +0700

    Add hero background

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,6 +4,17 @@ import teacherfind from "./teacherfind.png";
 import thecoffeehouse from "./thecoffeehouse.png";
 import hust from "./hust.png";
 import hieu from "./hieu.jpg";
+import herobg from "./herobg.png";
 import cv from "./cv/CV_Do_Minh_Hieu.pdf";
 import github from "./github.png";
-export { jinhome, teacherfind, grimm, thecoffeehouse, hust, cv, hieu, github };
+export {
+  jinhome,
+  teacherfind,
+  grimm,
+  thecoffeehouse,
+  hust,
+  cv,
+  hieu,
+  github,
+  herobg,
+};
